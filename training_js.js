e0bc409421780c46b90c310daa0aa093ccd83acb
Blacklivi <blacklivi@yandex.by>
2020-10-21 19:09:51 +0300

    new training

--- training_js.js
+++ training_js.js
@@ -1,3 +1,19 @@
-alert('Привет');
-console.log('test js');
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '')
+console.log(numberOfFilms)
 
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+}
+
+const a = prompt('Сколько фильмов просмотрел?', ''),
+      b = prompt('Сколько просмотрел', ''),
+      c = prompt('Сколько фильмов просмотрел?', ''),
+      d = prompt('Сколько просмотрел', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
